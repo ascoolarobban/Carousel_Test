a336b21e5448986411ba6d48cd2d1b0db934a091
ascoolarobban <robin@fauxdelorean.com>
2023-04-06 15:16:05 +0200

    Initial Commit

---
+++ Carousel_Test/Carousel_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Carousel_TestApp.swift
+//  Carousel_Test
+//
+//  Created by Robin Ellingsen on 2023-04-06.
+//
+
+import SwiftUI
+
+@main
+struct Carousel_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
